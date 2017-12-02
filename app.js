574526fd49ada76514cafef3ace64d8e15d9073f
moeabdol <mohd.a.saed@gmail.com>
2017-12-02 08:36:25 +0300

    Install handlebars engine and render home page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const morgan     = require('morgan');
 const axios      = require('axios');
 const async      = require('async');
+const hbs        = require('express-handlebars');
 
 const app = express();
 const PORT = 3000;
@@ -11,6 +12,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(morgan('dev'));
+
+app.engine('hbs', hbs({
+  defaultLayout: 'main',
+  extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
 
 app.listen(PORT, err => {
   if (err) return console.log(err);
